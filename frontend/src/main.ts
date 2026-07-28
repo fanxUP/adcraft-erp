5ea9ac7a2bfe361c8ca39b1a1d5c684292378eae
Admin <admin@adcraft.com>
2026-07-29 02:28:31 +0800

    修复报价删除确认框定位样式

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -7,6 +7,9 @@ import { createPinia } from 'pinia'
 import './styles/print.scss'
 import './styles/global.scss'
 import './styles/themes.scss'
+// MessageBox 通过函数调用创建，不经过模板自动按需加载，需显式引入其布局样式。
+import 'element-plus/es/components/message-box/style/css'
+import 'element-plus/theme-chalk/el-message-box.css'
 
 const app = createApp(App)
 
